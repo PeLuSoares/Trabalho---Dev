51f216c4d225200d43cb803737536f9b09ebe47a
Pedro Lucas Soares <pedroca23lucas@gmail.com>
2022-06-19 17:33:37 -0300

    New window creation function + it's images

--- Main.py
+++ Main.py
@@ -1,4 +1,9 @@
+
+from pydoc import doc
+from re import L, M
 from tkinter import *
+from tkinter import font
+from turtle import width
 import ttkbootstrap as ttk
 from ttkbootstrap.constants import *
 
@@ -17,19 +22,40 @@ img_bdel = PhotoImage(file="imgs\\del.png")
 img_bundo = PhotoImage(file="imgs\\undo.png")
 img_bsave = PhotoImage(file="imgs\\save.png")
 img_bmode = PhotoImage(file="imgs\\light.png")
+img_jan2bg = PhotoImage(file="imgs\\jan2bg.png")
 
-#Frame creation
-
-f_list = Frame(mainj, width=310, height=300, relief="flat")
-
-#Labels
+#Labels main
 lb_bg = Label(mainj, image=img_bg)
 lb_bg.pack()
+lb_li = Listbox(mainj, font=("Calibri 15 bold"), justify=CENTER,  bg="#caf6ff", width=75)
+lb_li.config(selectbackground="black", activestyle="none")
+lb_li.place(width=430, height=250, x=210, y=110)
+
+
+#Functions
+
+def popup():
+    #Window creation
+    jan2 = Toplevel()
+    jan2.title("Adicionar Afazeres")
+    jan2.geometry("400x200")
+    jan2.resizable(False, False)
+    jan2.transient(mainj)
+    jan2.focus_force()
+    jan2.grab_set()
+
+    #Label
+    lb_bg2 = Label(jan2, image=img_jan2bg)
+    lb_bg2.pack()
+    en_afazer = Entry(jan2, bd=2, font=("Calibri 12 bold"), justify=CENTER)
+    en_afazer.place(width=286, height=21, x=57, y=65)
+
+
 
 
 #Buttons
 
-b_new = Button(mainj, image=img_bnew, borderwidth=0)
+b_new = Button(mainj, image=img_bnew, borderwidth=0, command=popup)
 b_new.place(width=84, height=31, x=29, y = 130)
 
 b_del = Button(mainj, image=img_bdel, borderwidth=0)
@@ -46,5 +72,14 @@ b_undo.place(width=84, height=31, x=29, y = 290)
 b_mode = Button(mainj, image=img_bmode, bd=0, borderwidth=0)
 b_mode.place(width=84, height=31, x=29, y = 376.4)
 
+
+
+###################
+
+
+teste = ["Teste1", "Teste 2"]
+
+for item in teste:
+    lb_li.insert(END,item)
 
 mainj.mainloop()
